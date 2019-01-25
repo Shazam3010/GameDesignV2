a97c78cfdfa2265d939dd0247d1b202d24309bce
Jordan Collie <1406143@dundeeandangus.ac.uk>
2019-01-25 14:32:39 +0000

    first commit

---
+++ src/com/jordangames/main/Game.java
@@ -0,0 +1,46 @@
+package com.jordangames.main;
+
+import javax.swing.JFrame;
+
+public class Game 
+{
+    public static final int WINDOW_WIDTH = 800;
+    public static final int WINDOW_HEIGHT = 384;
+    public static final String TITLE = "New and Improved Game";
+    
+    private JFrame mainWindow;
+    private LevelPanel lp;
+    
+    public Game()
+    {
+        initGame();
+        initComponents();
+        initWindow();
+        
+        lp.startGame();
+    }
+    
+    private void initGame()
+    {
+        lp = new LevelPanel();
+        mainWindow = new JFrame();
+    }
+    
+    private void initComponents()
+    {
+        mainWindow.add(lp);
+    }
+    
+    private void initWindow()
+    {
+        mainWindow.setTitle(Game.TITLE);
+        mainWindow.setSize(Game.WINDOW_WIDTH, Game.WINDOW_HEIGHT);
+        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainWindow.setVisible(true);
+    }
+    
+    public static void main(String[] args) 
+    {
+        Game g =new Game();
+    }
+}
---
+++ src/gamedesignv2/GameDesignV2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gamedesignv2;
+
+/**
+ *
+ * @author 1406143
+ */
+public class GameDesignV2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        
+    }
+    
+}
